e51cd8afc0cdfb3d545a3e96f958df5fe2b5daf7
martin.kotevski <martin.kotevski@sorsix.com>
2026-03-23 06:15:43 +0100

    minor

--- queries/example_users.sql
+++
@@ -1,8 +0,0 @@
-select
-    id as id_nofilter,
-    TO_CHAR(date, 'YYYY-MM-DD') AS date,
-    TO_CHAR(date, 'Mon dd, yyyy') AS date_display,
-    amount * -1 as amount,
-    recipient_display as recipient,
-    '<a href="https://www.mozilla.org/en-US/">The Mozilla homepage</a>' as link_display_nosort_nofilter
-  from payments order by date desc;
